5f77aefbd99ab2e6ae580cd9a748a8c336cb0d92
Arjun Singh Tomar <109807311+arjundevlop@users.noreply.github.com>
2023-03-17 15:45:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function myPromise(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!");
+		},1000);
+	})
+}
+let outputDiv = document.getElementById("output");
+myPromise().then((res)=>{
+	outputDiv.innerHTML = res;
+})
